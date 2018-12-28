cee8bed6d869905ee4c26d8e5091cad83049a12a
fc373745 <fc373745@gmail.com>
2018-12-28 12:16:09 -0800

    added blackbox component with some errors

---
+++ src/components/BlackBox.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { scaleLinear, ScaleLinear } from 'd3-scale'
+import { axisBottom } from 'd3-axis'
+import { select } from 'd3-selection'
+
+class BlackBox extends React.Component {
+    private blackBox = React.createRef<SVGSVGElement>()
+    
+    componentDidMount() {
+        this.renderAxis()
+    }
+    componentDidUpdate() {
+        this.renderAxis()
+    }
+
+    renderAxis() {
+        const scale = scaleLinear()
+            .domain([0, 10])
+            .range([0, 200])
+
+        const bottomAxis = axisBottom(scale)
+
+        select(this.blackBox.current)
+            .call(bottomAxis)   
+    }
+
+    render(){
+        return (
+            <g transform="translate(10, 30)" ref={this.blackBox} />
+        )
+    }
+}
